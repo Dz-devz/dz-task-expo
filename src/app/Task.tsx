fdeaf5dadb2b9b83e142a20935854f9f37c0a6f1
Dz.dev <darwinbjordan@gmail.com>
2025-01-21 10:28:07 +0800

    chore: update ui of task page

--- src/app/Task.tsx
+++ src/app/Task.tsx
@@ -1,10 +1,12 @@
+import { Feather } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
 import { useFocusEffect } from "@react-navigation/native";
 import axios from "axios";
 import React, { useState } from "react";
 import {
+  ActivityIndicator,
   Alert,
-  Button,
+  Image,
   ScrollView,
   Text,
   TextInput,
@@ -24,12 +26,15 @@ type dataProps = {
 const Task = () => {
   const [data, setData] = useState<dataProps[]>([]);
   const [updateTask, setUpdateTask] = useState<dataProps | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
+    setLoading(true);
     try {
       const res = await axios.get("https://task-api-prod.up.railway.app/tasks");
       const body = await res.data;
       setData(body);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching tasks:", error);
     }
@@ -42,6 +47,7 @@ const Task = () => {
   );
 
   const handleUpdate = async (task: dataProps) => {
+    setLoading(true);
     const response = await axios.put(
       `https://task-api-prod.up.railway.app/tasks/${task.id}`,
       task
@@ -52,17 +58,20 @@ const Task = () => {
       setData((prev) =>
         prev.map((d) => (d.id === task.id ? { ...d, ...task } : d))
       );
+      setLoading(false);
       setUpdateTask(null);
     }
   };
 
   const handleDelete = async (id: string) => {
+    setLoading(true);
     const response = await axios.delete(
       `https://task-api-prod.up.railway.app/tasks/${id}`
     );
     if (response.status === 200) {
       Alert.alert("Delete", "Deleted successfully!");
       setData((prev) => prev.filter((task) => task.id !== id));
+      setLoading(false);
     }
   };
 
@@ -100,119 +109,166 @@ const Task = () => {
 
   return (
     <ScrollView className="flex-1 bg-gray-200">
-      <Text className="bg-stone-600 text-center font-bold text-2xl mt-4 text-white w-44 mx-auto">
-        List of Task
-      </Text>
-      <TouchableOpacity
-        className="w-32 h-8 mt-4 bg-pink-100 rounded-md mx-auto"
-        onPress={() => handleStatusFilter("PENDING")}
-      >
-        <Text className="font-semibold text-lg text-center">Pending</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className="w-32 h-8 mt-4 bg-pink-100 rounded-md mx-auto"
-        onPress={() => handleStatusFilter("PROCESSING")}
-      >
-        <Text className="font-semibold text-lg text-center">Processing</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className="w-32 h-8 mt-4 bg-pink-100 rounded-md mx-auto"
-        onPress={() => handleStatusFilter("COMPLETED")}
-      >
-        <Text className="font-semibold text-lg text-center">Completed</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className="w-32 h-8 mt-4 bg-pink-100 rounded-md mx-auto"
-        onPress={() => handleStatusFilter("CANCELLED")}
-      >
-        <Text className="font-semibold text-lg text-center">Cancelled</Text>
-      </TouchableOpacity>
-      {data.map((d) => (
-        <View
-          className="bg-rose-200 shadow my-4 p-4 rounded-2xl w-72 mx-auto"
-          key={d.id}
-        >
-          {updateTask?.id === d.id ? (
-            <View className="flex justify-center gap-2">
-              <TextInput
-                value={updateTask.task}
-                onChangeText={(text) =>
-                  setUpdateTask({ ...updateTask, task: text })
-                }
-                placeholder="Task"
-                className="bg-gray-200 p-2 rounded"
-              />
-              <TextInput
-                value={updateTask.description}
-                onChangeText={(text) =>
-                  setUpdateTask({ ...updateTask, description: text })
-                }
-                placeholder="Description"
-                className="bg-gray-200 p-2 rounded"
-              />
-              <Picker
-                style={{ backgroundColor: "#e5e7eb" }}
-                selectedValue={updateTask.status}
-                onValueChange={(text) =>
-                  setUpdateTask({ ...updateTask, status: text as TaskType })
-                }
-              >
-                <Picker.Item label="PENDING" value="PENDING" />
-                <Picker.Item label="PROCESSING" value="PROCESSING" />
-                <Picker.Item label="COMPLETED" value="COMPLETED" />
-                <Picker.Item label="CANCELLED" value="CANCELLED" />
-              </Picker>
-              <TextInput
-                value={updateTask.priority.toString()}
-                onChangeText={(text) =>
-                  setUpdateTask({
-                    ...updateTask,
-                    priority: parseInt(text) || 0,
-                  })
-                }
-                keyboardType="numeric"
-                placeholder="Priority"
-                className="bg-gray-200 p-2 rounded"
-              />
-              <Button title="Save" onPress={() => handleUpdate(updateTask)} />
-              <Button title="Cancel" onPress={() => setUpdateTask(null)} />
+      {loading && (
+        <ActivityIndicator
+          className="min-h-screen"
+          size="large"
+          color="black"
+        />
+      )}
+      {!loading && (
+        <>
+          {/* <Text className="bg-stone-600 text-center font-bold text-2xl mt-4 text-white w-44 mx-auto">
+            List of Task
+          </Text> */}
+          <Image
+            source={require("../assets/dz-task-high-resolution-logo-transparent.png")}
+            className="w-72 h-48 mx-auto mt-4"
+          />
+          <View className="flex flex-row mx-auto gap-2 mt-4">
+            <TouchableOpacity
+              className="w-32 h-8 bg-black/70 rounded-md mx-auto"
+              onPress={() => handleStatusFilter("PENDING")}
+            >
+              <Text className="font-semibold text-white text-lg text-center">
+                Pending
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="w-32 h-8 bg-black/70 rounded-md mx-auto"
+              onPress={() => handleStatusFilter("PROCESSING")}
+            >
+              <Text className="font-semibold text-white text-lg text-center">
+                Processing
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row mx-auto gap-2">
+            <TouchableOpacity
+              className="w-32 h-8 mt-2 bg-black/70 rounded-md mx-auto"
+              onPress={() => handleStatusFilter("COMPLETED")}
+            >
+              <Text className="font-semibold text-white text-lg text-center">
+                Completed
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="w-32 h-8 mt-2 bg-black/70 rounded-md mx-auto"
+              onPress={() => handleStatusFilter("CANCELLED")}
+            >
+              <Text className="font-semibold text-white text-lg text-center">
+                Cancelled
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {data.map((d) => (
+            <View
+              className="bg-rose-200/50 my-4 p-4 rounded-2xl w-80 mx-auto"
+              key={d.id}
+            >
+              {updateTask?.id === d.id ? (
+                <View className="flex justify-center gap-2">
+                  <TextInput
+                    value={updateTask.task}
+                    onChangeText={(text) =>
+                      setUpdateTask({ ...updateTask, task: text })
+                    }
+                    placeholder="Task"
+                    className="bg-gray-200 p-2 rounded w-[194px] ml-[15px]"
+                  />
+                  <TextInput
+                    value={updateTask.description}
+                    onChangeText={(text) =>
+                      setUpdateTask({ ...updateTask, description: text })
+                    }
+                    placeholder="Description"
+                    className="bg-gray-200 p-2 rounded w-[194px] ml-[15px]"
+                  />
+                  <Picker
+                    style={{
+                      backgroundColor: "#e5e7eb",
+                      width: 192,
+                      marginLeft: 16,
+                    }}
+                    selectedValue={updateTask.status}
+                    onValueChange={(text) =>
+                      setUpdateTask({ ...updateTask, status: text as TaskType })
+                    }
+                  >
+                    <Picker.Item label="PENDING" value="PENDING" />
+                    <Picker.Item label="PROCESSING" value="PROCESSING" />
+                    <Picker.Item label="COMPLETED" value="COMPLETED" />
+                    <Picker.Item label="CANCELLED" value="CANCELLED" />
+                  </Picker>
+                  <TextInput
+                    value={updateTask.priority.toString()}
+                    onChangeText={(text) =>
+                      setUpdateTask({
+                        ...updateTask,
+                        priority: parseInt(text) || 0,
+                      })
+                    }
+                    keyboardType="numeric"
+                    placeholder="Priority"
+                    className="bg-gray-200 p-2 rounded w-[194px] ml-[15px]"
+                  />
+                  <TouchableOpacity
+                    className="w-56 h-8 mt-4 bg-pink-50 rounded-md mx-auto"
+                    onPress={() => handleUpdate(updateTask)}
+                  >
+                    <Text className="font-semibold text-lg text-center">
+                      Save
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    className="w-56 h-8 mt-4 bg-pink-50 rounded-md mx-auto"
+                    onPress={() => setUpdateTask(null)}
+                  >
+                    <Text className="font-semibold text-lg text-center">
+                      Cancel
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ) : (
+                <View>
+                  <View className="flex justify-between items-center flex-row">
+                    <Text className="text-lg font-medium">{d.task}</Text>
+                    <Text
+                      className={`p-[2px] w-32 rounded-lg text-center text-white ${getStatusColor(
+                        d.status
+                      )}`}
+                    >
+                      {d.status}
+                    </Text>
+                  </View>
+                  <Text className="text-lg font-medium">{d.description}</Text>
+                  <Text className="text-lg font-medium">{d.priority}</Text>
+                  <View className="flex justify-between items-center flex-row">
+                    <Feather
+                      name="trash-2"
+                      size={24}
+                      color="black"
+                      onPress={() => handleDelete(d.id)}
+                    />
+                    <TouchableOpacity
+                      className="w-24 flex flex-row h-8 mt-4 gap-2"
+                      onPress={() => setUpdateTask(d)}
+                    >
+                      <Text className="font-semibold text-lg">Edit</Text>
+                      <Feather
+                        name="arrow-right"
+                        className="my-auto"
+                        size={24}
+                        color="black"
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
             </View>
-          ) : (
-            <View>
-              <View className="flex justify-between items-center flex-row">
-                <Text className="text-lg font-medium">{d.task}</Text>
-                <Text
-                  className={`p-[2px] w-32 rounded-lg text-center text-white ${getStatusColor(
-                    d.status
-                  )}`}
-                >
-                  {d.status}
-                </Text>
-              </View>
-              <Text className="text-lg font-medium">{d.description}</Text>
-              <Text className="text-lg font-medium">{d.priority}</Text>
-              <View className="flex justify-around items-center flex-row">
-                <TouchableOpacity
-                  className="w-24 h-8 mt-4 bg-pink-50 rounded-md"
-                  onPress={() => setUpdateTask(d)}
-                >
-                  <Text className="font-semibold text-lg text-center">
-                    Update
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  className="w-24 h-8 mt-4 bg-pink-50 rounded-md"
-                  onPress={() => handleDelete(d.id)}
-                >
-                  <Text className="font-semibold text-lg text-center">
-                    Delete
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          )}
-        </View>
-      ))}
+          ))}
+        </>
+      )}
     </ScrollView>
   );
 };
