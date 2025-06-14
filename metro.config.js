2a2f5a9c8d5a198bd34359d48fc75e9e3fd39226
Dz.dev <darwinbjordan@gmail.com>
2025-06-14 14:31:46 +0800

    chore: fix issue on export default

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
+// metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
 const config = getDefaultConfig(__dirname);
+
+// fix the export default issue/error
+// allow .cjs imports
+config.resolver.sourceExts.push("cjs");
+// <-- this line is the key fix
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
+
+const { withNativeWind } = require("nativewind/metro");
 
 module.exports = withNativeWind(config, { input: "./global.css" });
