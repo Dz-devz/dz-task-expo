114151e21bb93ae674f71ac0f24513a7c3efb051
Dz.dev <darwinbjordan@gmail.com>
2025-06-14 14:35:47 +0800

    chore: added ref on how i found the fix

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
 
+// Ref: https://github.com/expo/expo/discussions/35444
 // fix the export default issue/error
 // allow .cjs imports
 config.resolver.sourceExts.push("cjs");
